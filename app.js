e973dde2524708c6baf6034551f248ae0cef31ed
Whyetzz <75259101+WhyEtzz@users.noreply.github.com>
2022-04-03 17:57:01 +0700

    update app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+let express = require('express')
+const http = require('http')
+const app = express()
+const httpServer = http.createServer(app)
+const qrcode = require('qrcode')
+const { resizeImage } = require('./lib/converter') 
+global.qr = ' '
+
+app.set('json spaces', 2);
+app.use(express.json());
+app.get('/', async (req, res, next) => {
+    try {
+        res.setHeader("content-type", "image/png")
+        res.send(await resizeImage(await qrcode.toBuffer(global.qr), 512, 512))
+    } catch (error) {
+        res.send('err, ' + error.message)
+    }
+})
+
+const qrPrint = (qr) => {
+    app.get('/qr', async (req, res) => {
+        res.setHeader("content-type", "image/png")
+        res.send(await resizeImage(await qrcode.toBuffer(qr), 512, 512))
+    })
+}
+
+// Run the server
+const PORT = 3000
+httpServer.listen(PORT, () => {
+    console.log(color('[INFO] Web api Server on port: ', 'green') + color(`${PORT}`, 'yellow'))
+})
